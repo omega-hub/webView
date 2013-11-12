5796471336e83331d8170ee96cfa1c2e8047f666
Alessandro Febretti <febret@gmail.com>
2013-11-12 15:44:54 -0600

    webView now loads the browser.py module

--- webView.cpp
+++ webView.cpp
@@ -21,68 +21,68 @@ class WebView;
 class WebCore: public EngineModule
 {
 public:
-	static WebCore* instance();
+    static WebCore* instance();
 
-	WebCore();
-	virtual ~WebCore();
+    WebCore();
+    virtual ~WebCore();
 
-	WebView* createView(int width, int height);
-	void destroyView(WebView*);
-	virtual void update(const UpdateContext& context);
+    WebView* createView(int width, int height);
+    void destroyView(WebView*);
+    virtual void update(const UpdateContext& context);
 
 private:
-	static WebCore* mysInstance;
+    static WebCore* mysInstance;
 
-	Awesomium::WebCore* myCore;
-	List<WebView*> myViews;
+    Awesomium::WebCore* myCore;
+    List<WebView*> myViews;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
 class WebView: public PixelData
 {
 public:
-	// Static creation function, to keep consistent with the rest of the
-	// omegalib python API.
-	static WebView* create(int width, int height)
-	{ return WebCore::instance()->createView(width, height); }
+    // Static creation function, to keep consistent with the rest of the
+    // omegalib python API.
+    static WebView* create(int width, int height)
+    { return WebCore::instance()->createView(width, height); }
 
-	WebView(WebCore* core, Awesomium::WebView* internalView, int width, int height);
-	virtual ~WebView();
-	void resize(int width, int height);
-	void update();
-	void evaljs(const String& code);
+    WebView(WebCore* core, Awesomium::WebView* internalView, int width, int height);
+    virtual ~WebView();
+    void resize(int width, int height);
+    void update();
+    void evaljs(const String& code);
 
-	void loadUrl(const String& url);
-	void setZoom(int zoom);
-	int getZoom();
+    void loadUrl(const String& url);
+    void setZoom(int zoom);
+    int getZoom();
 
-	Awesomium::WebView* getInternalView()
-	{ return myView; }
+    Awesomium::WebView* getInternalView()
+    { return myView; }
 
 private:
-	Ref<WebCore> myCore;
-	Awesomium::WebView* myView;
-	int myWidth;
-	int myHeight;
+    Ref<WebCore> myCore;
+    Awesomium::WebView* myView;
+    int myWidth;
+    int myHeight;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
 class WebFrame: public Image
 {
 public:
-	static WebFrame* create(Container* container);
-	WebFrame(Engine* srv);
+    static WebFrame* create(Container* container);
+    WebFrame(Engine* srv);
 
-	virtual void handleEvent(const omega::Event& evt);
-	virtual void update(const omega::UpdateContext& context);
+    virtual void handleEvent(const omega::Event& evt);
+    virtual void update(const omega::UpdateContext& context);
 
-	void setView(WebView* view);
+    void setView(WebView* view);
 
-	WebView* getView() 
-	{ return myView; }
+    WebView* getView() 
+    { return myView; }
 
 private:
-	Ref<WebView> myView;
+    Ref<WebView> myView;
 };
 
 WebCore* WebCore::mysInstance = NULL;
@@ -91,211 +91,230 @@ WebCore* WebCore::mysInstance = NULL;
 // Python wrapper code.
 #ifdef OMEGA_USE_PYTHON
 #include "omega/PythonInterpreterWrapper.h"
+// Stores the compiled browser module (from browser.py)
+PyObject* sBrowserModule = NULL;
 BOOST_PYTHON_MODULE(webView)
 {
-	PYAPI_REF_CLASS(WebView, PixelData)
-		PYAPI_STATIC_REF_GETTER(WebView, create)
-		PYAPI_METHOD(WebView, loadUrl)
-		PYAPI_METHOD(WebView, resize)
-		PYAPI_METHOD(WebView, setZoom)
-		PYAPI_METHOD(WebView, getZoom)
-		PYAPI_METHOD(WebView, evaljs)
-		;
+    PYAPI_REF_CLASS(WebView, PixelData)
+        PYAPI_STATIC_REF_GETTER(WebView, create)
+        PYAPI_METHOD(WebView, loadUrl)
+        PYAPI_METHOD(WebView, resize)
+        PYAPI_METHOD(WebView, setZoom)
+        PYAPI_METHOD(WebView, getZoom)
+        PYAPI_METHOD(WebView, evaljs)
+        ;
 
-	PYAPI_REF_CLASS(WebFrame, Image)
-		PYAPI_STATIC_REF_GETTER(WebFrame, create)
-		PYAPI_REF_GETTER(WebFrame, getView)
-		PYAPI_METHOD(WebFrame, setView)
-		;
+    PYAPI_REF_CLASS(WebFrame, Image)
+        PYAPI_STATIC_REF_GETTER(WebFrame, create)
+        PYAPI_REF_GETTER(WebFrame, getView)
+        PYAPI_METHOD(WebFrame, setView)
+        ;
+
+    // Compile, load and import the euclid module.
+    String browserModuleSrc = DataManager::readTextFile("webView/browser.py");
+    if(browserModuleSrc != "")
+    {
+        PyObject* browserModuleCode = Py_CompileString(browserModuleSrc.c_str(), "browser", Py_file_input);
+        if(browserModuleCode != NULL)
+        {
+            sBrowserModule = PyImport_ExecCodeModule("browser", browserModuleCode);
+        }
+    }
+    else
+    {
+        owarn("Module webView: could not load extension module browser.py");
+    }
 }
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
 WebCore* WebCore::instance()
 {
-	if(!mysInstance)
-	{
-		mysInstance = new WebCore();
-		ModuleServices::addModule(mysInstance);
-		mysInstance->doInitialize(Engine::instance());
-	}
-	return mysInstance;
+    if(!mysInstance)
+    {
+        mysInstance = new WebCore();
+        ModuleServices::addModule(mysInstance);
+        mysInstance->doInitialize(Engine::instance());
+    }
+    return mysInstance;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 WebCore::WebCore()
 {
-	myCore = Awesomium::WebCore::Initialize( Awesomium::WebConfig() );
+    myCore = Awesomium::WebCore::Initialize( Awesomium::WebConfig() );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 WebCore::~WebCore()
 {
-	// NOTE: WebCore::Shutdown() crashes for some unknown reason. Leaving this
-	// commented will cause a leak, but hopefully we are doing this during app
-	// shutdown, so it's not too bad.
-	//Awesomium::WebCore::Shutdown();
-	myCore = NULL;
-	mysInstance = NULL;
+    // NOTE: WebCore::Shutdown() crashes for some unknown reason. Leaving this
+    // commented will cause a leak, but hopefully we are doing this during app
+    // shutdown, so it's not too bad.
+    //Awesomium::WebCore::Shutdown();
+    myCore = NULL;
+    mysInstance = NULL;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 WebView* WebCore::createView(int width, int height)
 {
-	Awesomium::WebView* internalView = myCore->CreateWebView(width, height);
-	WebView* view = new WebView(this, internalView, width, height);
-	myViews.push_back(view);
-	return view;
+    Awesomium::WebView* internalView = myCore->CreateWebView(width, height);
+    WebView* view = new WebView(this, internalView, width, height);
+    myViews.push_back(view);
+    return view;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void WebCore::destroyView(WebView* view)
 {
-	oassert(view);
-	myViews.remove(view);
+    oassert(view);
+    myViews.remove(view);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void WebCore::update(const UpdateContext& context)
 {
-	myCore->Update();
-	foreach(WebView* view, myViews)
-	{
-		view->update();
-	}
+    myCore->Update();
+    foreach(WebView* view, myViews)
+    {
+        view->update();
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 WebView::WebView(WebCore* core, Awesomium::WebView* internalView, int width, int height):
 PixelData(PixelData::FormatRgba, width, height, 0),
-	myCore(core),
-	myView(internalView),
-	myWidth(width),
-	myHeight(height)
+    myCore(core),
+    myView(internalView),
+    myWidth(width),
+    myHeight(height)
 {
-	myView->SetTransparent(true);
+    myView->SetTransparent(true);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 WebView::~WebView()
 {
-	WebCore::instance()->destroyView(this);
-	myView = NULL;
+    WebCore::instance()->destroyView(this);
+    myView = NULL;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void WebView::resize(int width, int height)
 {
-	myWidth = width;
-	myHeight = height;
-	myView->Resize(width, height);
-	PixelData::resize(width, height);
+    myWidth = width;
+    myHeight = height;
+    myView->Resize(width, height);
+    PixelData::resize(width, height);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void WebView::loadUrl(const String& url)
 {
-	Awesomium::WebURL urlData(Awesomium::WSLit(url.c_str()));
-	myView->LoadURL(urlData);
-	myView->Focus();
+    Awesomium::WebURL urlData(Awesomium::WSLit(url.c_str()));
+    myView->LoadURL(urlData);
+    myView->Focus();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void WebView::update()
 {
-	Awesomium::BitmapSurface* surface = static_cast<Awesomium::BitmapSurface*>(myView->surface());
-	if(surface && surface->is_dirty())
-	{
-		unsigned char* ptr = map();
-		surface->CopyTo(ptr, myWidth * 4, 4, true, true);
-		unmap();
-		setDirty();
-	}
+    Awesomium::BitmapSurface* surface = static_cast<Awesomium::BitmapSurface*>(myView->surface());
+    if(surface && surface->is_dirty())
+    {
+        unsigned char* ptr = map();
+        surface->CopyTo(ptr, myWidth * 4, 4, true, true);
+        unmap();
+        setDirty();
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void WebView::setZoom(int zoom)
 {
-	myView->SetZoom(zoom);
+    myView->SetZoom(zoom);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 int WebView::getZoom()
 {
-	return myView->GetZoom();
+    return myView->GetZoom();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void WebView::evaljs(const String& code)
 {
-	myView->ExecuteJavascript(Awesomium::WSLit(code.c_str()), Awesomium::WebString());
+    myView->ExecuteJavascript(Awesomium::WSLit(code.c_str()), Awesomium::WebString());
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 WebFrame* WebFrame::create(Container* container)
 {
-	WebFrame* frame = new WebFrame(Engine::instance());
-	container->addChild(frame);
-	return frame;
+    WebFrame* frame = new WebFrame(Engine::instance());
+    container->addChild(frame);
+    return frame;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 WebFrame::WebFrame(Engine* srv):
-	Image(srv)
+    Image(srv)
 {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void WebFrame::setView(WebView* view)
 {
-	myView = view;
-	setData(myView);
+    myView = view;
+    setData(myView);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void WebFrame::handleEvent(const omega::Event& evt)
 {
-	if(myView != NULL)
-	{
-		if(isPointerInteractionEnabled())
-		{
-			if(evt.getServiceType() == Event::ServiceTypePointer || evt.getServiceType() == Event::ServiceTypeWand)
-			{
-				Vector2f point  = Vector2f(evt.getPosition().x(), evt.getPosition().y());
-				point = transformPoint(point);
-				if(simpleHitTest(point))
-				{
-					Awesomium::WebView* w = myView->getInternalView();
+    if(myView != NULL)
+    {
+        if(isPointerInteractionEnabled())
+        {
+            if(evt.getServiceType() == Event::ServiceTypePointer || evt.getServiceType() == Event::ServiceTypeWand)
+            {
+                Vector2f point  = Vector2f(evt.getPosition().x(), evt.getPosition().y());
+                point = transformPoint(point);
+                if(simpleHitTest(point))
+                {
+                    Awesomium::WebView* w = myView->getInternalView();
 
-					w->InjectMouseMove(point[0], point[1]);
-					if(evt.isButtonDown(UiModule::getClickButton()))		
-					{
-						w->InjectMouseDown(Awesomium::kMouseButton_Left);
-						evt.setProcessed();
-					}
-					else if(evt.isButtonUp(UiModule::getClickButton())) // Need to check buton up like this because mouse service takes away button flag on up button events.
-					{
-						w->InjectMouseUp(Awesomium::kMouseButton_Left);
-						evt.setProcessed();
-					}
-				}
-			}
-		}
-	}
+                    w->InjectMouseMove(point[0], point[1]);
+                    if(evt.isButtonDown(UiModule::getClickButton()))		
+                    {
+                        w->InjectMouseDown(Awesomium::kMouseButton_Left);
+                        evt.setProcessed();
+                        EventSharingModule::markLocal(evt);
+                    }
+                    else if(evt.isButtonUp(UiModule::getClickButton())) // Need to check buton up like this because mouse service takes away button flag on up button events.
+                    {
+                        w->InjectMouseUp(Awesomium::kMouseButton_Left);
+                        evt.setProcessed();
+                        EventSharingModule::markLocal(evt);
+                    }
+                }
+            }
+        }
+    }
 
-	Image::handleEvent(evt);
+    Image::handleEvent(evt);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void WebFrame::update(const omega::UpdateContext& context)
 {
-	Widget::update(context);
-	if(myView != NULL)
-	{
-		if(myView->getHeight() != getHeight() ||
-			myView->getWidth() != getWidth())
-		{
-			setSize(Vector2f(myView->getWidth(), myView->getHeight()));
-		}
-	}
+    Widget::update(context);
+    if(myView != NULL)
+    {
+        if(myView->getHeight() != getHeight() ||
+            myView->getWidth() != getWidth())
+        {
+            setSize(Vector2f(myView->getWidth(), myView->getHeight()));
+        }
+    }
 }
